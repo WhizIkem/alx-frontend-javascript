09535e3a0ffe6573d661352c758329ca2e489164
whizikem <wisdomikem@gmail.com>
2023-07-25 00:48:34 +0100

    ES6 method properties

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,16 @@
+import getBudgetObject from "./7-getBudgetObject.js";
+
+export default function getFullBudget(income, gdp, capita) {
+    const budget = getBudgetObject(income, gdp, capita);
+    const fullBudget = {
+        ...budget,
+        getInomeInDollars(income) {
+            return `$${income}`;
+        },
+        getIncomeInEuros(income) {
+            return `€${income}`;
+        },
+    };
+
+    return fullBudget;
+}

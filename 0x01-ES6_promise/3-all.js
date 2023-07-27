aeb12991e5f3337b7517d714fc7f20d144400789
whizikem <wisdomikem@gmail.com>
2023-07-27 14:30:14 +0100

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,16 +1,9 @@
 import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
-  const promises = [
-    uploadPhoto(),
-    createUser(),
-  ];
-  Promise.all(promises)
+  return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
-      const [photo, user] = values;
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline')
-    });
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+    }) 
+    .catch(() => console.log('Signup system offline'));
 }

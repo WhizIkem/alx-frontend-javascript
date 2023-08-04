2d3b1f0aa388130f20ab901265fac64ae0dbccc0
whizikem <wisdomikem@gmail.com>
2023-08-04 14:49:00 +0100

    lint error fixed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,16 +3,16 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUpUserPromise = signUpUser(firstName, lastName)
-  .then(
-    (value) => ({ status: 'fulfilled', value }),
-    (error) => ({ status: 'rejected', value: error })
-  );
+    .then(
+      (value) => ({ status: 'fulfilled', value }),
+      (error) => ({ status: 'rejected', value: error })
+    );
 
-  const uploadPhotoPromise = uploadPhoto(fileName)
-  .then(
-    (value) => ({ status: 'fulfilled', value }),
-    (error) => ({ status: 'rejected', value: error })
-  );
+    const uploadPhotoPromise = uploadPhoto(fileName)
+    .then(
+      (value) => ({ status: 'fulfilled', value }),
+      (error) => ({ status: 'rejected', value: error }),
+    );
 
   return Promise.all([signUpUserPromise, uploadPhotoPromise]);
 }

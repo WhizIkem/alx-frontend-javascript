86b315aece9337073ab9312a4c9897fcbea3b9be
whizikem <wisdomikem@gmail.com>
2023-09-16 00:33:37 +0100

    Typed Arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,6 @@ function createInt8TypedArray(length, position, value) {
     throw new Error('Position outside range');
   }
 
-
   const buffer = new ArrayBuffer(length);
   const view = new Int8Array(buffer);
   view[position] = value;

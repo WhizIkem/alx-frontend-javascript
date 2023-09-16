ff6c9462e2fd23933e426b5f025c42b7a4d9354a
whizikem <wisdomikem@gmail.com>
2023-09-16 18:34:27 +0100

    Creating an interface for student

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -25,15 +25,24 @@ document.addEventListener("DOMContentLoaded", () => {
   const table = document.createElement('table');
   const tbody = document.createElement('tbody');
 
+  table.style.borderCollapse = 'collapse';
+  table.style.width = '50%';
+  table.style.marginTop = '20px';
+  table.style.border = '1px solid black';
+
   studentsList.forEach((student) => {
     const row = document.createElement('tr');
 
     const firstNameCell = document.createElement('td');
     firstNameCell.textContent = student.firstName;
+    firstNameCell.style.border = '1px solid black';
+    firstNameCell.style.padding = '8px';
     row.appendChild(firstNameCell);
 
     const locationCell = document.createElement('td');
     locationCell.textContent = student.location;
+    locationCell.style.border = '1px solid black';
+    locationCell.style.padding = '8px';
     row.appendChild(locationCell);
 
     tbody.appendChild(row);

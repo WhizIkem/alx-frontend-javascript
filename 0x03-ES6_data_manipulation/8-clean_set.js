34fb0e795a0e49f7caecb568203cfb823bc7dcb8
whizikem <wisdomikem@gmail.com>
2023-09-16 10:54:26 +0100

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,18 +1,14 @@
-function cleanSet(set, startString) {
-  if (typeof startString !== 'string' || startString === '') return '';
+export default function cleanSet(set, startString) {
+  let text = '';
+  const array = [];
 
-  let result = [];
-
-  for (let item of set) {
-    if (item.startsWith(startString)) {
-      const substring = item.slice(startString.length);
-      if (substring) {
-        result.push(substring);
+  if (startString && typeof startString === 'string') {
+    for (const element of set) {
+      if (element && element.startsWith(startString)) {
+        array.push(element.slice(startString.length));
       }
     }
+    text = array.join('-');
   }
-
-  return result.join('-');
+  return text;
 }
-
-export default cleanSet;

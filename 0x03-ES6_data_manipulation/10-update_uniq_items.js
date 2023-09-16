7d7ba78ea604b455c5aef8462611e757062a05ef
whizikem <wisdomikem@gmail.com>
2023-09-16 11:29:55 +0100

    More map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,15 @@
+function updateUniqueItems(groceriesMap) {
+  if (!(groceriesMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  groceriesMap.forEach((value, key) => {
+    if (value === 1) {
+      groceriesMap.set(key, 100);
+    }
+  });
+
+  return groceriesMap;
+}
+
+export default updateUniqueItems;

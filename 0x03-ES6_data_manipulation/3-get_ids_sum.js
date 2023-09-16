df06764b5c122c0c3ac8be966fd7790898a611b9
whizikem <wisdomikem@gmail.com>
2023-09-16 09:20:19 +0100

    Reduce

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,5 @@
 function getStudentIdSum(students) {
-  return students.reduce((accumulator, student) => accumulator + student.id, 0)
+  return students.reduce((accumulator, student) => accumulator + student.id, 0);
 }
 
 export default getStudentIdSum;

251ff086707747678823ed3771e646ceaa5d1d6b
whizikem <wisdomikem@gmail.com>
2023-09-17 22:42:12 +0100

    Brand convention & Nominal typing

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,23 +1,19 @@
-interface MajorCredits {
+export interface MajorCredits {
   credits: number;
-  _majorBrand: unknown;
+  _majorCreditBrand: void;
 }
 
-interface MinorCredits {
+export interface MinorCredits {
   credits: number;
-  _minorBrand: unknown;
+  _minorCreditBrand: void;
 }
 
-function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
   return {
-    credits: subject1.credits + subject2.credits,
-    _majorBrand: undefined
-  };
+    credits: subject1.credits + subject2.credits } as MajorCredits;
 }
 
 function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
   return {
-    credits: subject1.credits + subject2.credits,
-    _minorBrand: undefined
-  };
+    credits: subject1.credits + subject2.credits } as MinorCredits;
 }

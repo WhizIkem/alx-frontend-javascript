8e4d0394f8f686e5150205b453ba4cfd565bcde6
whizikem <wisdomikem@gmail.com>
2025-07-13 12:28:55 +0100

    Extending the Teacher class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -22,3 +22,17 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+// extend teacher interface to Directors interface and add numberOfReports property
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: "John",
+  lastName: "Doe",
+  location: "London",
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+}
+console.log(director1);
